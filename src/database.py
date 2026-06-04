c73dc3c3e30b1efa4511d078adc9ef75aca09c87
Vlad <vovanikolaev140707@gmail.com>
2026-06-04 22:56:45 +0300

    added alembic

--- src/database.py
+++ src/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
 from src.settings import settings as s
+from src.models.models import Base
 
 DATABASE_URL = f"postgresql+asyncpg://{s.DB_USER}:{s.DB_PASSWORD}@{s.DB_HOST}:{s.DB_PORT}/{s.DB_NAME}"
 
@@ -11,3 +12,6 @@ async_session = async_sessionmaker(
     expire_on_commit=False
 )
 
+# async def init_db():
+#     async with async_engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.create_all)
